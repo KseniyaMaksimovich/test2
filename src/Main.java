64adc971d29b3f3e6d5beff0147bd375f5c0ba9e
Kseniya Maksimovich <ksuvorova4@gmail.com>
2024-05-03 16:18:38 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        Knight knight = new Knight();
+        knight.goAndSaveThePrincess();
+    }
+}
+    class Knight {
+        private String name = "Sir Thanks-A-Lot";
+        private String weapon = "Long Sword";
+        private Boolean isGoingToSavePrincess = true;
+
+        public static void main(String[] args) {
+
+        }
+
+            public void goAndSaveThePrincess () {
+                sharpenBlade();
+                getFood();
+                assembleTeam();
+                getTransport();
+                System.out.println("Da idu uzhe.....");
+            }
+
+            private void sharpenBlade () {
+                System.out.println("Tochim mech");
+            }
+
+            private void getFood () {
+                System.out.println("Sobiraem konservy");
+            }
+
+            private void assembleTeam () {
+                System.out.println("Budim oruzhenosca");
+            }
+
+            private void getTransport () {
+                System.out.println("Sedlaem konia");
+            }
+    }
